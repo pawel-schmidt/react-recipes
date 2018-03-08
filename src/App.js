70b1f811dcaf95427cf17633cfeab8ed5c844965
wiqoo <wikweb@gmail.com>
2018-03-08 15:45:52 +0100

    separate component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Ingredients from "./components/Ingredients";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Ingredients />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
