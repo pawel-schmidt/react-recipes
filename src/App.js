7eedd28ae84838a10132c07b4958c2fa9c6fc227
Pawel Schmidt <px.sxxx@gmail.com>
2018-03-08 16:56:14 +0100

    Added filtering

--- src/App.js
+++ src/App.js
@@ -1,28 +1,52 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 import Ingredients from "./components/Ingredients";
 import RecipesList from "./components/RecipesList";
 
 import allIngredients from "./data/ingredients.json";
 import allRecipes from "./data/recipes.json";
 
+const ALL_CATEGORIES = "All";
 class App extends Component {
   constructor(props) {
     super(props);
+    const allCategoriesWithDuplicates = [
+      ALL_CATEGORIES,
+      ...allRecipes.map(recipe => recipe.category)
+    ];
+    const allCategories = [...new Set(allCategoriesWithDuplicates)];
     this.state = {
       allIngredients,
       allRecipes,
+      allCategories,
       visibleIngredients: allIngredients,
-      visibleRecipes: allRecipes
+      visibleRecipes: allRecipes,
+      selectedCategory: ""
     };
+  }
+
+  filterRecipes(categoryName) {
+    this.setState({
+      visibleRecipes:
+        categoryName === ALL_CATEGORIES
+          ? this.state.allRecipes
+          : this.state.allRecipes.filter(
+              recipe => recipe.category === categoryName
+            )
+    });
   }
 
   render() {
     return (
       <div className="App">
         <Ingredients />
-        <RecipesList visibleRecipes={this.state.visibleRecipes} />
+        <RecipesList
+          allCategories={this.state.allCategories}
+          selectedCategory={this.state.selectedCategory}
+          visibleRecipes={this.state.visibleRecipes}
+          filterRecipes={this.filterRecipes.bind(this)}
+        />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
--- src/components/RecipesList.js
+++ src/components/RecipesList.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
-const Ingredients = ({visibleRecipes}) => (
-  <ul>
-    {visibleRecipes.map(recipe => <li>{recipe.name}</li>)}
-  </ul>
+const Ingredients = ({allCategories, visibleRecipes, filterRecipes}) => (
+  <div>
+    <h2>Categories</h2>
+    <ul>
+      {allCategories.map(category => (
+        <li key={category}>
+          <button onClick={() => filterRecipes(category)}>{category}</button>
+        </li>
+      ))}
+    </ul>
+    <ul>
+      {visibleRecipes.map(recipe => <li key={recipe.id}>{recipe.name}</li>)}
+    </ul>
+  </div>
 );
 
 export default Ingredients;
