f185f43297ec5f7aba20137030b2f47f86dbd88e
Pawel Schmidt <px.sxxx@gmail.com>
2018-03-08 17:57:44 +0100

    Removed template code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import Ingredients from "./components/Ingredients";
 import RecipesList from "./components/RecipesList";
@@ -64,13 +63,6 @@ class App extends Component {
           visibleRecipes={this.state.visibleRecipes}
           filterRecipes={this.filterRecipes.bind(this)}
         />
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
