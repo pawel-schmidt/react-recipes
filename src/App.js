01f4d342fd3e42c71b90f49e1dc4e0fad88b1099
Pawel Schmidt <px.sxxx@gmail.com>
2018-03-16 00:27:26 +0700

    Added search ingredients

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ class App extends Component {
       allCategories,
       visibleIngredients: allIngredients,
       selectedIngredients: [],
+      searchText: "",
       selectedCategory: ALL_CATEGORIES
     };
   }
@@ -58,6 +59,24 @@ class App extends Component {
     }));
   }
 
+  onSearchTextChange(event) {
+    this.setState({
+      searchText: event.target.value,
+      visibleIngredients: this.state.allIngredients.filter(ingredient =>
+        this.progressiveSearch(event.target.value, ingredient)
+      )
+    });
+  }
+
+  progressiveSearch(searchPhrase, string) {
+    let index = -1;
+    const stringLowerCase = string.toLowerCase();
+    return searchPhrase.toLowerCase().split("").reduce((allCharsFulfillCondition, char) => {
+      index = stringLowerCase.indexOf(char, index + 1);
+      return allCharsFulfillCondition && (index !== -1);
+    }, true);
+  }
+
   filterRecipes(selectedCategory) {
     this.setState({ selectedCategory });
   }
@@ -69,6 +88,8 @@ class App extends Component {
           visibleIngredients={this.state.visibleIngredients}
           selectedIngredients={this.state.selectedIngredients}
           addIngredient={this.addIngredient.bind(this)}
+          onSearchTextChange={this.onSearchTextChange.bind(this)}
+          searchText={this.state.searchText}
         />
         <RecipesList
           allCategories={this.state.allCategories}
--- src/components/Ingredients.js
+++ src/components/Ingredients.js
@@ -1,19 +1,30 @@
-import React from 'react';
-import './Ingredients.css';
+import React from "react";
+import "./Ingredients.css";
 
-const Ingredients = ({visibleIngredients, selectedIngredients, addIngredient}) => {
-    return (
-		<div>
-			<p>Dodaj składniki:</p>
-			<ul className="Ingredients-list">
-				{visibleIngredients.map(ingredient => <li onClick={() => addIngredient(ingredient)} key={ingredient}>{ingredient}</li>)}
-			</ul>
-			<h1>Wybrane przez Ciebie składniki</h1>
-			<ul className="Ingredients-list">
-				{selectedIngredients.map(ingredient => <li key={ingredient}>{ingredient}</li>)}
-			</ul>
-		</div>
-	);
-}
+const Ingredients = ({
+  visibleIngredients,
+  selectedIngredients,
+  addIngredient,
+  onSearchTextChange,
+  searchText
+}) => (
+  <div>
+    <p>Dodaj składniki:</p>
+    <input value={searchText} onChange={onSearchTextChange} />
+    <ul className="Ingredients-list">
+      {visibleIngredients.map(ingredient => (
+        <li onClick={() => addIngredient(ingredient)} key={ingredient}>
+          {ingredient}
+        </li>
+      ))}
+    </ul>
+    <h1>Wybrane przez Ciebie składniki</h1>
+    <ul className="Ingredients-list">
+      {selectedIngredients.map(ingredient => (
+        <li key={ingredient}>{ingredient}</li>
+      ))}
+    </ul>
+  </div>
+);
 
 export default Ingredients;
